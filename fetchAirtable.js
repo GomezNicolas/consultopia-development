ff2112bbdfc788b8eec50cefc6c9078bc6e7277d
Nicolas Gomez Colchero <gomez@Nicolass-MacBook-Pro.local>
2023-07-31 17:56:33 +0200

    Finally been able to fix it, this code fetches data and replaces variable with data that has been sent.

--- fetchAirtable.js
+++ fetchAirtable.js
@@ -11,13 +11,9 @@ const response = await axios.get(`https://api.airtable.com/v0/${airtableBaseId}/
 const records = _.get(response, 'data.records', [])
 const phoneN = workflow.phoneN
 
-const matchingRecord = records.find(record => record.fields.phoneN === phoneN)
+const matchingRecord = records.find(record => record.fields.Phone === phoneN)
 
 if (matchingRecord) {
-  workflow.userData = matchingRecord.fields.userData
-  workflow.name = matchingRecord.fields.name
-  workflow.checkin = luxon.DateTime.fromISO(matchingRecord.fields.checkin)
-  workflow.checkout = luxon.DateTime.fromISO(matchingRecord.fields.checkout)
-  workflow.roomName = matchingRecord.fields.roomName
-  workflow.createdTime = matchingRecord.fields.createdTime
+  workflow.roomName = matchingRecord.fields['Room Name']
+  // this should allow the variable to be accessed later in the workflow.
 }
